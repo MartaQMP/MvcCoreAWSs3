26f40b2335a932c370a817b97273707602970d9a
MartaQMP <martaquirosmp@gmail.com>
2026-05-11 14:18:43 +0200

    Add project files.

---
+++ MvcCoreAWSs3/Controllers/AwsFilesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreAWSs3.Services;
+using System.Threading.Tasks;
+
+namespace MvcCoreAWSs3.Controllers
+{
+    public class AwsFilesController : Controller
+    {
+        private ServiceStorageS3 service;
+
+        public AwsFilesController(ServiceStorageS3 service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<string> files = await this.service.GetFilesAsync();
+            ViewBag.Mnesaje = TempData["Mensaje"];
+            return View(files);
+        }
+
+        public async Task<IActionResult> UploadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            int codigo = 0;
+            using(Stream stream = file.OpenReadStream())
+            {
+                codigo = await this.service.UploadFileAsync(file.FileName, stream);
+            }
+            TempData["Mensaje"] = "Status Code: " + codigo;
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> DeleteFile(string fileName)
+        {
+            await this.service.DeleteFileAsync(fileName);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcCoreAWSs3/Services/ServiceStorageS3.cs
@@ -0,0 +1,55 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using System.Net;
+
+namespace MvcCoreAWSs3.Services
+{
+    public class ServiceStorageS3
+    {
+        private string BucketName;
+
+        private IAmazonS3 clientS3;
+
+        public ServiceStorageS3 (IConfiguration configuration, IAmazonS3 clientS3)
+        {
+            this.BucketName = configuration.GetValue<string>("AWS:BucketName");
+            this.clientS3 = clientS3;
+        }
+
+        // METODO PARA SUBIR FICHEROS
+        public async Task<int> UploadFileAsync(string fileName, Stream stream)
+        {
+            PutObjectRequest request = new PutObjectRequest
+            {
+                Key = fileName,
+                BucketName = this.BucketName,
+                InputStream = stream,
+            };
+            PutObjectResponse response = await this.clientS3.PutObjectAsync(request);
+            if(response.HttpStatusCode == HttpStatusCode.OK)
+            {
+                // AQUI HARIAMOS LO QUE FUERA..
+            }
+            int code = (int)response.HttpStatusCode;
+            return code;
+        }
+
+        public async Task DeleteFileAsync(string fileName)
+        {
+            DeleteObjectResponse response = await this.clientS3.DeleteObjectAsync(this.BucketName, fileName);
+        }
+
+        /* METODO PARA RECUPERAR TODOS LOS FICHEROS
+         * DEBEMOS INDICAR LA VERION AUNQ NO TENGAMOS */
+        public async Task<List<string>> GetFilesAsync()
+        {
+            ListVersionsResponse response = await this.clientS3.ListVersionsAsync(this.BucketName);
+            if(response.Versions == null)
+            {
+                return null;
+            }
+            /* EXTRAEMOS LAS KEYS (FILENAME) POR DEFECTO NOS DEVUELVE LA ULTIMA VERSION */
+            return response.Versions.Select(f => f.Key).ToList();
+        }
+    }
+}
